95dc088f8b9ff7d36f046e70faa426706eb38153
arushinarayan <67413254+arushinarayan@users.noreply.github.com>
2021-04-09 14:07:49 +0530

    Update random_quote_machine.js

--- random_quote_machine.js
+++ random_quote_machine.js
@@ -3,5 +3,17 @@ async function getquote(){
   const data = await response.json()
   const text = document.querySelector('#quotetext')
   text.innerHTML = (`${data.content} —${data.author}`)
+
+  $('#tweet-button').attr(
+    'href',
+    'https://twitter.com/intent/tweet?&related=freecodecamp&text=' +
+      encodeURIComponent('"' + data.content + '" -' + data.author)
+  );
+
+  $('#fb-share-button').attr(
+    'href',
+    'https://facebook.com/sharer.php?display=popup&u=https://arushinarayan.github.io/randomquotemachine/&p[quote]=' +encodeURIComponent('"' + data.content + '" -' + data.author)
+  );
 }
 getquote();
+
